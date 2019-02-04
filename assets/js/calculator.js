06923eb5115bb6f6767a6af8b1406ea4f7c82645
Adam Culpepper <me@adamculpepper.net>
2019-02-04 00:14:58 -0600

    Adding IDs to all the elements that have values that'll change and adding those IDs in calculator.js to get them updated to test they're updating

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,3 +1,29 @@
 $(function() {
 
+	//Title Services
+	$('#settlement-fee').text('updated');
+	$('#abstract-fee').text('updated');
+	$('#document-preperation').text('updated');
+	$('#total-title-services').text('updated');
+	
+	//Title Insurance
+	$('#lenders-title-insurance').text('updated');
+	$('#advanced-owners-title-insurance').text('updated');
+	$('#alta-8-1-endorsement').text('updated');
+	$('#alta-9-endorsement').text('updated');
+	$('#closing-protection-letter').text('updated');
+	$('#total-title-insurance').text('updated');
+
+	//Recording Fees
+	$('#cash-sale-deed').text('updated');
+	$('#mortgage').text('updated');
+	$('#total-recording-fees').text('updated');
+
+	//Total TItle Fees
+	$('#total-title-fees').text('updated');
+
+	//Estimated Property
+	$('#all-other-parishes-with-homestead-exception').text('updated');
+	$('#all-other-parishes-without-homestead-exception').text('updated');
+
 });
