6a79bd658036faa4978da5cc991a79df78b83e25
Adam Culpepper <me@adamculpepper.net>
2019-03-09 17:50:06 -0600

    Hooking up the Information block and Title Services block

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,6 +1,18 @@
 $(function() {
 
+
+
 	//Title Services
+	// var settlementFee = $('#settlement-fee').val();
+	// var abstractFee = $('#abstract-fee').val();
+	// var documentPreperation = $('#document-preperation').val();
+	// var totalTitleServices = $('#total-title-services').val();
+
+	var settlementFee = 0;
+	var abstractFee = 0;
+	var documentPreperation = 0;
+	var totalTitleServices = 0;
+
 	$('#settlement-fee').text('updated');
 	$('#abstract-fee').text('updated');
 	$('#document-preperation').text('updated');
@@ -26,4 +38,66 @@ $(function() {
 	$('#all-other-parishes-with-homestead-exception').text('updated');
 	$('#all-other-parishes-without-homestead-exception').text('updated');
 
+	$('#updatable-inputs').on('keyup', 'input', function(e) {
+		updateValues();
+	});
+
+	$('#updatable-inputs').on('change', 'select', function(e) {
+		updateValues();
+	});
+
+	updateValues();
+
+	function updateValues() {
+		var transactionType = $('#transaction-type').val();
+		var salesPrice = $('#sales-price').val();
+		var loanAmount = $('#loan-amount').val();
+		var propertyParish = $('#property-parish').val();
+
+		if (transactionType == 'purchase') {
+			settlementFee = 395;
+			abstractFee = 220;
+			documentPreperation = 235;
+		} else if (transactionType == 'refinance') {
+			settlementFee = 395;
+			abstractFee = 130;
+			documentPreperation = 235;
+		} else if (transactionType == 'cash') {
+			settlementFee = 635;
+			abstractFee = '<i>Included</i>';
+			documentPreperation = '<i>Included</i>';
+		} else {
+			console.warn('updateValues() error');
+		}
+
+		console.warn('settlementFee: ' + settlementFee);
+		$('#settlement-fee').html(settlementFee);
+		$('#abstract-fee').html(abstractFee);
+		$('#document-preperation').html(documentPreperation);
+		
+		if (transactionType == 'cash') {
+			$('#total-title-services').text(settlementFee);
+		} else {
+			$('#total-title-services').text(settlementFee + abstractFee + documentPreperation);
+		}
+	}
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
